c1b7951aea19bd91bae77150ac8f9c87ac1a1c47
piotr <ptr.dopierala@gmail.com>
2020-01-14 11:54:33 +0100

    Working java 8

---
+++ GaeFlexJ11Example/src/main/java/pl/dopierala/HomeGaeFlexJ11Example/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package pl.dopierala.HomeGaeFlexJ11Example.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello from GaeExtdTest J11";
+    }
+}
