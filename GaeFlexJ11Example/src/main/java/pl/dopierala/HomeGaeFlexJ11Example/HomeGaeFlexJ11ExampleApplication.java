2d99c926ac906f7f169ca8e45232cfdcdc296239
piotr <ptr.dopierala@gmail.com>
2020-01-14 11:55:41 +0100

    Working java 8

---
+++ GaeFlexJ11Example/src/main/java/pl/dopierala/HomeGaeFlexJ11Example/HomeGaeFlexJ11ExampleApplication.java
@@ -0,0 +1,13 @@
+package pl.dopierala.HomeGaeFlexJ11Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeGaeFlexJ11ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeGaeFlexJ11ExampleApplication.class, args);
+	}
+
+}
---
+++ GaeFlexJ11Example/src/test/java/pl/dopierala/HomeGaeFlexJ11Example/HomeGaeFlexJ11ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dopierala.HomeGaeFlexJ11Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeGaeFlexJ11ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
